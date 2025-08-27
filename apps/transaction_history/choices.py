93c3b6b0f183176bdd4a84df125a1ff75f81f8bf
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-08-27 14:23:16 +0900

    ✨ Add models
    add models

---
+++ apps/transaction_history/choices.py
@@ -0,0 +1,12 @@
+TRANSACTION_METHOD = [
+    ("ATM", "ATM 거래"),
+    ("TRANSFER", "계좌이체"),
+    ("AUTOMATIC_TRANSFER", "자동이체"),
+    ("CARD", "카드결제"),
+    ("INTEREST", "이자"),
+]
+
+TRANSACTION_TYPE = [
+    ("DEPOSIT", "입금"),
+    ("WITHDRAW", "출금"),
+]
