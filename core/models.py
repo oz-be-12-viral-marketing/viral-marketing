deeeda073635f201c9467a6585424551fdaa0fef
dev-s-kr <zkzktldk1234@gmail.com>
2025-08-28 15:01:10 +0900

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,11 +1,1 @@
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
-
-class User(AbstractUser):
-    nickname = models.CharField(max_length=255, unique=True, blank=True, null=True)
-    phone_number = models.CharField(max_length=20, unique=True, blank=True, null=True)
-
-    def __str__(self):
-        return self.email or self.username
