8d6002b6034a1255c3008c9137a1fbc6b054ee8e
BE12-OZ <dhwmzheld6470@gmail.com>
2025-08-28 16:55:17 +0900

    fix: Resolve workflow errors and update dependencies

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,16 +7,16 @@ SECRET_KEY = "django-insecure-development-key"
 
 ALLOWED_HOSTS = ["*"]
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "testdb",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "HOST": "db",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "testdb",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
 
 # Copy all settings from base.py
 for setting in dir(base):
