919cbace8781c941ef877ef09fac41113016c3c6
BE12-OZ <dhwmzheld6470@gmail.com>
2025-08-28 14:40:04 +0900

    feat: Implement account and transaction APIs

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -24,4 +24,6 @@ urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/v1/", include("apps.accounts.urls")),
+    path("api/v1/", include("apps.transaction_history.urls")),
 ]
