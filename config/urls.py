27269910c56a4f8524db759a51b413ec020c9158
dev-s-kr <zkzktldk1234@gmail.com>
2025-09-01 18:35:36 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -37,7 +37,5 @@ if settings.DEBUG:
     import debug_toolbar
     from django.conf.urls.static import static
 
-    urlpatterns += [
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-        path("__debug__/", include(debug_toolbar.urls)),
-    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
