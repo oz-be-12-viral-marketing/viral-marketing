c1a54a832519b98b67cf058a9bcc6d93c213bd72
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-09-01 10:43:17 +0900

    🐛 Fix analysis.signals code
    fix analysis.signals code

--- apps/analysis/signals.py
+++ apps/analysis/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from .models import AnalysisResult
+from .models import Analysis
 
 
-@receiver(post_delete, sender=AnalysisResult)
+@receiver(post_delete, sender=Analysis)
 def cleanup_analysis(sender, instance, **kwargs):
-    print(f"AnalysisResult {instance.id} 이 성공적으로 삭제되었습니다.")
+    print(f"Analysis {instance.id} 이 성공적으로 삭제되었습니다.")
