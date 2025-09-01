d72f8c7b5b917d7a1fc4969403f83f757cea27dc
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-09-01 13:15:12 +0900

    🐛 Fix config.urls
    fix config.urls

--- config/urls.py
+++ config/urls.py
@@ -35,7 +35,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+    from django.conf.urls.static import static
 
     urlpatterns += [
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
         path("__debug__/", include(debug_toolbar.urls)),
     ]
