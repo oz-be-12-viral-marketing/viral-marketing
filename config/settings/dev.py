d7374582eb6736075a07c03786578010be928dbb
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-09-02 11:03:27 +0900

    🐛 Fix style
    fix style

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,11 +1,12 @@
 # config/settings/dev.py
 # 개발 환경용 설정
 import os
+
 from .base import *  # noqa: F403
 
 DEBUG = True
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-development-key") # Load from env var
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-development-key")  # Load from env var
 
 ALLOWED_HOSTS = ["*"]
 
@@ -14,12 +15,10 @@ ROOT_URLCONF = "config.urls"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "testdb"),
-        "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "NAME": os.environ.get("DB_NAME", "mydb"),
+        "USER": os.environ.get("DB_USER", "myuser"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "mypassword"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
-
-
